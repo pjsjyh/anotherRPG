1c3333443ccc62b8d854ab4c86a6f78cb139e124
pjsjyh <pjsjyh05@gmail.com>
2024-11-23 22:30:23 +0900

    서버 코드 수정

--- 3dMMO/Assets/scripts/Server/ApiUtilities.cs
+++ 3dMMO/Assets/scripts/Server/ApiUtilities.cs
@@ -2,8 +2,8 @@ namespace ApiUtilities
 {
     public class ApiUrls
     {
-        //public static string BaseUrl = System.Environment.GetEnvironmentVariable("RPG_GAME_URL");
-        public static string BaseUrl = "http://localhost:8080";
+        public static string BaseUrl = System.Environment.GetEnvironmentVariable("RPG_GAME_URL");
+        //public static string BaseUrl = "http://localhost:8080";
         public static string SetUrl = BaseUrl + "/set";
 
         public static string RegisterUrl = BaseUrl + "/register";
