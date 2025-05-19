38feb86f93f74366bfebed22f3877a80dd61f06d
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2025-05-20 00:30:26 +0900

    Update CharacterRepository.cs

--- 3dMMO/Assets/scripts/GameComponent/Player/CharacterRepository.cs
+++ 3dMMO/Assets/scripts/GameComponent/Player/CharacterRepository.cs
@@ -14,11 +14,11 @@ public class CharacterRepository : MonoBehaviour
         {
             string json = JsonConvert.SerializeObject(characterManager, Formatting.Indented);
             File.WriteAllText(saveFilePath, json);
-            //Debug.Log("✅ 캐릭터 데이터 저장 완료: " + saveFilePath);
+            //Debug.Log("캐릭터 데이터 저장 완료: " + saveFilePath);
         }
         catch (System.Exception e)
         {
-            Debug.LogError("❌ 캐릭터 데이터 저장 실패: " + e.Message);
+            Debug.LogError("캐릭터 데이터 저장 실패: " + e.Message);
         }
     }
 
@@ -33,10 +33,10 @@ public class CharacterRepository : MonoBehaviour
             }
             catch (System.Exception e)
             {
-                Debug.LogError("❌ 캐릭터 데이터 불러오기 실패: " + e.Message);
+                Debug.LogError("캐릭터 데이터 불러오기 실패: " + e.Message);
             }
         }
-        Debug.LogWarning("⚠ 저장된 캐릭터 데이터가 없음. 새 데이터 생성");
+        Debug.LogWarning("저장된 캐릭터 데이터가 없음. 새 데이터 생성");
         return null;
     }
 }
